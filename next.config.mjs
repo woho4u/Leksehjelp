7249bc89bb70b0e85404d2abec172293d1ad5ff2
Oliver Eriksen <okebedrift@gmail.com>
2024-10-30 10:33:11 +0100

    Remove deploy scripts from package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/leksehjelp/' : '',
+  basePath: isProd ? '/leksehjelp' : '',
+  output: 'export'
+};
 
 export default nextConfig;
